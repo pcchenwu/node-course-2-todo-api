548d2baf2556eefc0962959cd786219d186b9ecc
陳吳柏俊 <pcchenwu@chenwubjundeMBP.lan>
2017-07-06 11:05:28 +0800

    Add /GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,17 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos,
+            status: '5566 is good'
+        }); 
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000, () => {
     console.log('Starting on port 3000');
 });
