c47627190002c540a9b8ef72593ac2b93df64de9
陳吳柏俊 <pcchenwu@chenwubojundeMacBook-Pro.local>
2017-07-12 18:14:40 +0800

    Add GET /users/me and authentication middleware

--- server/model/User.js
+++ server/model/User.js
@@ -54,6 +54,26 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc');
+    } catch (e) {
+    //   return new Promise((resolve, reject) => {
+    //       reject();
+    //   });
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'token.token': token,
+        'token.access': 'auth'
+    });
+}
+
 var User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
