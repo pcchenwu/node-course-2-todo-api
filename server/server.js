889fed4bb5b206cce2932fdad0268c404b50c19c
陳吳柏俊 <pcchenwu@chenwubojundeMacBook-Pro.local>
2017-07-12 17:02:42 +0800

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -94,7 +94,22 @@ app.patch('/todos/:id', (req,res) => {
         res.status(400).send();
     });
 
-})
+});
+
+app.post('/users', (req, res) => {
+    // console.log(req.body);
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+    
+    user.save().then(() => {
+        return user.generateAuthToken();
+        // res.send(doc);
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Starting on port ${port}`);
