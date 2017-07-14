bb96edeb1fc0be2c443f481f1cb2fb87e996e52e
陳吳柏俊 <pcchenwu@chenwubjundeMBP.lan>
2017-07-14 11:07:38 +0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -118,6 +118,17 @@ app.get('/users/me', authenticate, (req, res) => {
    
 });
 
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+          res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Starting on port ${port}`);
 });
