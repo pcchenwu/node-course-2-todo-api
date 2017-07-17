88cf5bf9398498117d02954733b7ac0b6b638f21
陳吳柏俊 <pcchenwu@chenwubojundeMacBook-Pro.local>
2017-07-17 14:14:14 +0800

    Update config to use json file

--- server/model/User.js
+++ server/model/User.js
@@ -46,7 +46,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc').toString();
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET).toString();
 
     user.token.push({ access, token });
 
@@ -70,7 +70,7 @@ UserSchema.statics.findByToken = function (token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         return Promise.reject();
     }
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -12,7 +12,7 @@ const users = [{
     password: 'userOnePass',
     token: [{
         access: 'auth',
-        token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc').toString()
+        token: jwt.sign({ _id: userOneId, access: 'auth' }, process.env.JWT_SECRET).toString()
     }]
 }, {
     _id: userTwoId,
@@ -20,7 +20,7 @@ const users = [{
     password: 'userTwoPass',
     token: [{
         access: 'auth',
-        token: jwt.sign({ _id: userTwoId, access: 'auth' }, 'abc').toString()
+        token: jwt.sign({ _id: userTwoId, access: 'auth' }, process.env.JWT_SECRET).toString()
     }]
 }];
 
